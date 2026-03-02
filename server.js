3d1c167ab936b2934eaf0529b4828c1734755191
Jonathan Sierra <jonathangalindo1101@gmail.com>
2026-03-02 18:25:02 -0500

    cambio index

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.render("login", { title: "Login" });
+});
+
 app.get("/dashboard", (req, res) => {
   res.render("dashboard", { title: "Dashboard", active: "dashboard" });
 });
